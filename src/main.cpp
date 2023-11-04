90e2cf27f344f1e6604b2d79ad7997591bda611f
ewoudenberg-kasisto <eric.woudenberg@kasisto.com>
2023-11-04 15:33:41 -0400

    Begin adding in IR Remote handling. Debounce IR remote input.

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@
 #define IDLE_TIME_SECONDS 10 // One hour: 3600
 #define BUTTON_HOLD_MS 250 // The duration of a button press on the remote.
 
-#define SENSOR_PIN 2
+#define PIR_SENSOR_IN 2
+#define IR_REMOTE_IN 12
 #define RELAY_PIN_0 3
 #define BUTTON_COUNT 6
 
@@ -26,10 +27,12 @@ void pressRandomButton();
 int getRandomPress();
 bool isMotionDetected();
 void loadRandomPresses();
+void IRsetup();
+void IRloop();
 
 void setup() {
-  // Set PIR sensor pin to input
-  pinMode(SENSOR_PIN, INPUT);
+  IRsetup();
+  pinMode(PIR_SENSOR_IN, INPUT);
   // Set relay pins to output and turn them off
   for (int i = 0; i < BUTTON_COUNT; i++) {
     pinMode(getRelayPin(i), OUTPUT);
@@ -41,12 +44,13 @@ void setup() {
 }
 
 void loop() {
+  IRloop();
   if (isMotionDetected())
     pressRandomButton();
 }
 
 bool isMotionDetected() {
-  return !isIdled() && digitalRead(SENSOR_PIN) == HIGH;
+  return !isIdled() && digitalRead(PIR_SENSOR_IN) == HIGH;
 }
 
 int getRandomPress() {
@@ -99,4 +103,97 @@ void setRelay(int relayNr, int state) {
 
 bool isIdled() {
   return millis() < LastTriggerTime + IdleTimeMs;
+}
+
+#include <IRremote.h>
+
+const int IR_RECEIVE_PIN = 12;  // Define the pin number for the IR Sensor
+String lastDecodedValue = "";  // Variable to store the last decoded value
+String decodeKeyValue(long result);
+
+void IRsetup() {
+  Serial.begin(9600);                                     // Start serial communication at 9600 baud rate
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);  // Start the IR receiver
+}
+
+long LastIrReceived = millis();
+#define IR_DEBOUNCE_MS 120
+
+int getDebouncedIRCode() {
+  if (IrReceiver.decode()) {
+    int code = IrReceiver.decodedIRData.command;
+    IrReceiver.resume();  // Enable receiving of the next value
+
+    long deltaMs = millis() - LastIrReceived;
+    LastIrReceived = millis();
+    if (deltaMs > IR_DEBOUNCE_MS)
+      return code;
+  }
+  return 0;
+}
+
+void IRloop() {
+  int irCode = getDebouncedIRCode();
+  if (irCode) {
+    String decodedValue = decodeKeyValue(irCode);
+    Serial.print(irCode);
+    if (decodedValue != "ERROR") {
+      Serial.print(" ");
+      Serial.print(decodedValue);
+    }
+    Serial.println();
+  }
+}
+
+
+String decodeKeyValue(long result)
+{
+  switch(result){
+    case 0x16:
+      return "0";
+    case 0xC:
+      return "1"; 
+    case 0x18:
+      return "2"; 
+    case 0x5E:
+      return "3"; 
+    case 0x8:
+      return "4"; 
+    case 0x1C:
+      return "5"; 
+    case 0x5A:
+      return "6"; 
+    case 0x42:
+      return "7"; 
+    case 0x52:
+      return "8"; 
+    case 0x4A:
+      return "9"; 
+    case 0x9:
+      return "+"; 
+    case 0x15:
+      return "-"; 
+    case 0x7:
+      return "EQ"; 
+    case 0xD:
+      return "U/SD";
+    case 0x19:
+      return "CYCLE";         
+    case 0x44:
+      return "PLAY/PAUSE";   
+    case 0x43:
+      return "FORWARD";   
+    case 0x40:
+      return "BACKWARD";   
+    case 0x45:
+      return "POWER";   
+    case 0x47:
+      return "MUTE";   
+    case 0x46:
+      return "MODE";       
+    case 0x0:
+      return "ERROR";   
+    default :
+      return "ERROR";
+    }
 }
